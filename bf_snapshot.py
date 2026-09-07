7ffb06ec959f362393aa36003c7ed576ed5c0b42
bin <githhhh@users.noreply.github.com>
2026-09-07 15:46:32 +0800

    Restore snapshot calendar exactly to main

--- bf_snapshot.py
+++ bf_snapshot.py
@@ -64,7 +64,7 @@ def _easter_sunday(year: int) -> date:
     f = (b + 8) // 25
     g = (b - f + 1) // 3
     h = (19 * a + b - d - g + 15) % 30
-    i = b % 4
+    i = b // 4
     k = c % 4
     l = (32 + 2 * e + 2 * i - h - k) % 7
     m = (a + 11 * h + 22 * l) // 451
